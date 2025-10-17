b270573c3ce47ea0afe9e0ee19c03f139a4d2087
BrunoTakeo <bruno_takeo19@hotmail.com>
2025-10-17 16:45:24 -0300

    Interação Menu: Teclas cima, baixo e enter

--- code/Menu.py
+++ code/Menu.py
@@ -3,7 +3,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import WIN_WIDTH, COLOR_RED, MENU_OPTIONS, COLOR_WHITE, COLOR_GRAY
+from code.Const import WIN_WIDTH, COLOR_RED, MENU_OPTIONS, COLOR_WHITE, COLOR_GRAY, COLOR_YELLOW
 
 
 class Menu:
@@ -15,6 +15,7 @@ class Menu:
 
 
     def run(self):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.flac')
         pygame.mixer_music.play(-1)
         while True:
@@ -22,7 +23,11 @@ class Menu:
             self.menu_text(60, "Aero Mission", COLOR_RED, ((WIN_WIDTH / 2), 90))
 
             for i in range(len(MENU_OPTIONS)):
-                self.menu_text(30, MENU_OPTIONS[i], COLOR_GRAY, ((WIN_WIDTH / 2), 200 + 60 * i))
+                if i == menu_option:
+                    self.menu_text(30, MENU_OPTIONS[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 60 * i))
+                else:
+                    self.menu_text(30, MENU_OPTIONS[i], COLOR_GRAY, ((WIN_WIDTH / 2), 200 + 60 * i))
+
             pygame.display.flip()
 
         # Checar todos os eventos
@@ -30,6 +35,21 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit() #fechar janela
                     quit() # fim pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTIONS) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTIONS) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTIONS[menu_option]
+
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple, outline_color=(0, 0, 0), outline_thickness=2):
         text_font: Font = pygame.font.SysFont('Trebuchet MS', text_size)
